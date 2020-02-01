03f17498837dd9a7922076b2bfcc7b7e87c66eef
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-01 14:45:27 +0100

    refactored

---
+++ src/main/java/arrays/ExerciseNames.java
@@ -0,0 +1,40 @@
+package arrays;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class ExerciseNames {
+    public static void main(String[] args) {
+        String[] names = new String[]{"Ala", "Ola", "Andrzej", "Brunhilda", "Gienia", "Bożydar", "Gniewomir", "Sławomir", "Mścisław", "Stanisław", "Bronisław", "Wiercisław", "Petronela", "Kunegunda"};
+        /*
+        1. wypisz imiona żeńskie -a
+        2. imiona żeńskie nie dł↓ższe niż 3
+        3. imiona słowiańskie -mir oraz -sław
+        4.najdłuższe imię
+         */
+        System.out.println("Imiona żeńskie to: ");
+        for (String name : names) {
+            if (name.endsWith("a")) {
+                System.out.println(name);
+            }
+        }
+        System.out.println("Imiona żeńskie nie dłuższe niż 3 znaki to: ");
+        for (String name : names) {
+            if (name.endsWith("a") && name.length() <= 3) {
+                System.out.println(name);
+            }
+        }
+        System.out.println("Imiona męskie to: ");
+        for (String name : names) {
+            if (name.endsWith("mir") || name.endsWith("sław")) {
+                System.out.println(name);
+            }
+        }
+        StringBuffer longestName = new StringBuffer();
+        for (String name : names) {
+            if (name.length() > longestName.length()) {
+                longestName.replace(0, longestName.length(), name);
+            }
+        }
+        System.out.println("Najdłuższe imię to: " + longestName);
+    }
+}
