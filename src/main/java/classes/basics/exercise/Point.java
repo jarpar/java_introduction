dde20268cb5a5402e00042f8f98125e27e0fb9bf
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-02 12:01:57 +0100

    refactored

--- src/main/java/classes/basics/exercise/Point.java
+++ src/main/java/classes/basics/exercise/Point.java
@@ -11,4 +11,16 @@ public class Point {
         this.y = y;
     }
 
+    void coordinates() {
+        System.out.println("X " + x + " Y " + y);
+    }
+
+    static double distance(Point point1, Point point2) {
+        return Math.sqrt(Math.pow(point1.x - point2.x, 2) + (Math.pow(point1.y - point2.y, 2)));
+    }
+
+    double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + (Math.pow(this.y - other.y, 2)));
+    }
 }
+
---
+++ src/main/java/classes/basics/exercise/Test.java
@@ -0,0 +1,17 @@
+package classes.basics.exercise;
+
+public class Test {
+    public static void main(String[] args) {
+        Point point1 = new Point(3.0D, 1.0D);
+        Point point2 = new Point(4.0D, 7.0D);
+
+        double distance = Point.distance(point1, point2);
+        System.out.println(distance);
+
+        distance = point1.distance(point2);
+        System.out.println(distance);
+
+        distance = point2.distance(point1);
+        System.out.println(distance);
+    }
+}
