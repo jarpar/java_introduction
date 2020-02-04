27fe4684eb86b19c6d79c0e4ffa930ab3071c47f
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-04 19:13:12 +0100

    fixed for different and asymetrical sizes

--- src/main/java/arrays/ChessBoard.java
+++ src/main/java/arrays/ChessBoard.java
@@ -2,7 +2,7 @@ package arrays;
 
 import java.util.Scanner;
 
-public class ChessBoard {
+class ChessBoard {
     public static void main(String[] args) {
         ChessBoard chessBoard = new ChessBoard();
         System.out.println(chessBoard);
