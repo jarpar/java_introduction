e1f405f6129cfeb8266c4f7544ae46086a785d38
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-09 14:23:39 +0100

    `refactored`

---
+++ src/main/java/regexp/RegexpTest.java
@@ -0,0 +1,13 @@
+package regexp;
+
+public class RegexpTest {
+    private static final String log = "64.242.88.10 - - [07/Aug/2004:16:05:49 -0800] \"GET /twiki/bin/edit/StringTest1/Double_bounce_sender?topicparent=StringTest1.ConfigurationVariables HTTP/1.1\" 401 12846\n"
+            + "192.168.88.22 - - [07/Apr/2004:16:06:51 -0800] \"GET /twiki/bin/rdiff/TWiki/NewUserTemplate?rev1=1.3&rev2=1.2 HTTP/1.1\" 200 4523\n"
+            + "64.242.88.10 - - [07/May/2004:16:10:02 -0800] \"GET /mailman/listinfo/hsdivision HTTP/1.1\" 200 6291\n"
+            + "192.168.8.11 - - [07/Dec/2004:16:11:58 -0800] \"GET /twiki/bin/view/TWiki/WikiSyntax HTTP/1.1\" 200 7352\n"
+            + "64.242.88.12 - - [07/Nov/2004:16:20:55 -0800] \"GET /twiki/bin/view/StringTest1/DCCAndPostFix HTTP/1.1\" 200 5253";
+
+    public static void main(String[] args) {
+
+    }
+}
