c6a7fceccab3b4a65d9e335ee8c5e4639f952133
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-09 14:31:43 +0100

    `refactored`

--- src/main/java/regexp/RegexpTest.java
+++ src/main/java/regexp/RegexpTest.java
@@ -1,5 +1,8 @@
 package regexp;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexpTest {
     private static final String log = "64.242.88.10 - - [07/Aug/2004:16:05:49 -0800] \"GET /twiki/bin/edit/StringTest1/Double_bounce_sender?topicparent=StringTest1.ConfigurationVariables HTTP/1.1\" 401 12846\n"
             + "192.168.88.22 - - [07/Apr/2004:16:06:51 -0800] \"GET /twiki/bin/rdiff/TWiki/NewUserTemplate?rev1=1.3&rev2=1.2 HTTP/1.1\" 200 4523\n"
@@ -8,6 +11,10 @@ public class RegexpTest {
             + "64.242.88.12 - - [07/Nov/2004:16:20:55 -0800] \"GET /twiki/bin/view/StringTest1/DCCAndPostFix HTTP/1.1\" 200 5253";
 
     public static void main(String[] args) {
-
+        Pattern ipPattern = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
+        Matcher ipMatcher = ipPattern.matcher(log);
+        while (ipMatcher.find()) {
+            System.out.println(ipMatcher.group());
+        }
     }
 }
