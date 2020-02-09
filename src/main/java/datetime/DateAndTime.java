1a52befc6208c5620d20715595d7989ad7595d58
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-09 11:23:08 +0100

    `refactored`

--- src/main/java/datetime/DateAndTime.java
+++ src/main/java/datetime/DateAndTime.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public class DateAndTime {
     public static void main(String[] args) {
         LocalDateTime currentTime = LocalDateTime.now();
-        System.out.println(currentTime);
+        System.out.println("Current date and time: " + currentTime);
+
     }
 }
